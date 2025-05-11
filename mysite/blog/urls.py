c50d43a26aeb86cda02ebf7b07ccbd327e5c8d6e
moroz <morozovdaniel713@gmail.com>
2025-05-11 15:20:03 +0300

    feat(blog): add initional urls paths

---
+++ mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('', views.post_list, name='post_list'),
+]
