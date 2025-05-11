88f2ad75e8f88db15892f14af756d540496b7e87
moroz <morozovdaniel713@gmail.com>
2025-05-11 15:09:38 +0300

    feat(blog)L add initional views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,22 @@
+from django.http import Http404
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+
+def post_list(request):
+
+    posts = Post.published.all()
+
+    data = {"posts": posts}
+    return render(request, "blog/post/list.html", data)
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+        data = {"post": post}
+
+    except Post.DoesNotExist:
+        raise Http404("No post found.")
+
+    return render(request, "blog/post/detail.html", context=data)
