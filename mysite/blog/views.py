a88bd130b32e4cc01376b7a45e0bc981560c26fd
moroz <morozovdaniel713@gmail.com>
2025-05-11 15:10:02 +0300

    feat(blog): update views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
@@ -12,11 +12,9 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-        data = {"post": post}
-
-    except Post.DoesNotExist:
-        raise Http404("No post found.")
+    
+    post = get_object_or_404(Post, 
+                                    id=id, status=Post.Status.PUBLISHED)
+    data = {"post": post}
 
     return render(request, "blog/post/detail.html", context=data)
