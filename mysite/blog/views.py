a3d90f5756e9fdbd1e5ff1c3bf295551461f62c9
danila <146642624+wpotoke@users.noreply.github.com>
2025-05-12 01:27:11 +0300

    feat(blog): add post pagination (#3)
    * feat(blog): add paginator.
    
    * feat(templates): add paginator.html and use it in templates
    
    * feat(blog): add handling exceptions.
    
    * feat(blog): update handling exceptions.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
@@ -5,9 +6,19 @@ from blog.models import Post
 
 def post_list(request):
 
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     data = {"posts": posts}
+
     return render(request, "blog/post/list.html", data)
 
 
