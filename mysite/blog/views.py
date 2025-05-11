90fe8fda97abd3d7a085cac00fde46016f364e53
moroz <morozovdaniel713@gmail.com>
2025-05-12 00:56:46 +0300

    feat(blog): add paginator.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
@@ -5,9 +6,13 @@ from blog.models import Post
 
 def post_list(request):
 
-    posts = Post.published.all()
+    post_list = Post.published.all()
 
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     data = {"posts": posts}
+    
     return render(request, "blog/post/list.html", data)
 
 
