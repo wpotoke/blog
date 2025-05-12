6d2f145c6e21831aa9b3ae9cb3648751106f6f5b
moroz <morozovdaniel713@gmail.com>
2025-05-12 19:44:24 +0300

    fix(blog): update comment model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -58,7 +58,7 @@ class Comment(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField()
+    active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['created']
