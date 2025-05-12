532a44d6d1fdff2159a7c6d311f565f4ff4272a3
moroz <morozovdaniel713@gmail.com>
2025-05-12 22:07:49 +0300

    feat(blog): import class TaggableManager and aply migrations

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -31,6 +32,7 @@ class Post(models.Model):
         max_length=2, choices=Status.choices, default=Status.DRAFT
     )
     slug = models.SlugField(max_length=250, unique_for_date="publish")
+    tags = TaggableManager()
 
     class Meta:
         ordering = ["-publish"]
