550b5acafee0ca3703b838aa59f70754f5036ec1
danila <146642624+wpotoke@users.noreply.github.com>
2025-05-12 19:56:32 +0300

    Feature/comment system (#7)
    * feat(blog): add model and register it in admin.py
    
    * feat(blog): add model form (CommentModel)
    
    * feat(blog): add view for comment
    
    * feat(blog): add urls for comment.
    
    * feat(blog): update post_detail, add comment and form in view
    
    * feat(templates): add include comment_form and comment templates
    
    * feat(templates): update detail.html
    
    * fix(blog): update comment model
    
    * fix(blog): update migration
    
    * fix(blog): update form comment
    
    * chore(blog): del unusedspace in view

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 class EmailForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -6,3 +7,8 @@ class EmailForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+    
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
